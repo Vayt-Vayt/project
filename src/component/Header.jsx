d67f88fde6974787b5fb4794ae95d70eecdc2895
Vayt-Vayt <vayt08979@gmail.com>
2022-07-17 17:13:11 +0400

    project creation

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="app-header">
+            <img alt='логотип страницы' src='https://is1-ssl.mzstatic.com/image/thumb/Purple124/v4/0d/1d/20/0d1d2041-0fa4-cc63-4fce-8a90071aa510/source/512x512bb.jpg' width='40px' />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='app-nav'>
+            <div>
+              <a href='null'>Profile</a>
+            </div>
+            <div>
+              <a href='null'>Profile</a>
+            </div>
+            <div>
+              <a href='null'>Profile</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
