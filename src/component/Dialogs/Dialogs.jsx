7f267e97372346da66c5d5f4323d727bf456be43
Vayt-Vayt <vayt08979@gmail.com>
2022-07-18 11:37:17 +0400

    setings router

--- src/component/Dialogs/Dialogs.jsx
+++ src/component/Dialogs/Dialogs.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import classes from './Dialogs.module.css'
+import {NavLink} from 'react-router-dom';
+import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            
+        <div className={classes.dialogs}>
+            <div className={classes.dialogs_items}>
+                <div className={classes.dialog}>
+                       <NavLink to='/dialogs/1'>Name1</NavLink> 
+                    </div>
+                    <div className={classes.dialog}>
+                       <NavLink to='/dialogs/2'>Name2</NavLink> 
+                    </div>
+                 <div className={classes.dialog}>
+                 <NavLink to='/dialogs/3'>Name3</NavLink> 
+                 </div>
+            </div> 
+             <div className={classes.messages}>
+                <div className={classes.message}>
+                    hellov
+                </div>
+                <div className={classes.message}>
+                    hellov
+                </div>
+                <div className={classes.message}>
+                    hellov
+                </div>
+             </div>
         </div>
     );
 };
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom'
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
-    return (
-        <nav className={classes.nav}>
-            <div className={classes.item}>
-              <a href='/profile'>Profile</a>
-            </div>
-            <div className={classes.item}>
-              <a href='/dialogs'>Dialogs</a>
-            </div>
-            <div className={classes.item}>
-              <a href='/news'>News</a>
-            </div>
-            <div className={classes.item}>
-              <a href='/music'>Music</a>
-            </div>
-            <div className={classes.item}>
-              <a href='/setings'>Setings</a>
-            </div>
-        </nav>
-    );
+  const clas = navData => navData.isActive ? classes.active : classes.item;
+  return (
+      <nav className={classes.nav}>
+          <div className={classes.item}>
+            <NavLink to='/profile' className={ clas } >Profile</NavLink>
+          </div>
+          <div className={classes.item}>
+            <NavLink to='/dialogs' className={ clas } >Dialogs</NavLink>
+          </div>
+          <div className={classes.item}>
+            <NavLink to='/news' className={ clas } >News</NavLink>
+          </div>
+          <div className={classes.item}>
+            <NavLink to='/music' className={ clas } >Music</NavLink>
+          </div>
+          <div className={classes.item}>
+            <NavLink to='/setings' className={ clas } >Setings</NavLink>
+          </div>
+      </nav>
+  );
 };
 
 export default Navbar;
